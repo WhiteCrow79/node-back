7f85920db51fc79db8cbd2f12cecdd3d84ab2161
whitecrow <whitecrow@whitecrowui-Macmini.local>
2021-04-26 23:06:21 +0900

    하드코딩

--- app.js
+++ app.js
@@ -22,11 +22,39 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('여기는 루트입니다.');
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        여기는 루트입니다.
+    </body>
+    </html>
+    `);
 });
 
 app.get('/login', (req, res) => {
-    res.send('여기는 로그인 화면 입니다.');
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <input type="text" placeholder="아이디"><br>
+        <input type="text" placeholder="비밀번호"><br>
+        <button>로그인</button>
+    </body>
+    </html>
+    `);
 })
 
 app.listen(3000, () => console.log('서버 가동'));
