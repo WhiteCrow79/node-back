59742f02280238bd57ddd0e03aa88a4dc5745189
whitecrow <whitecrow@whitecrowui-Macmini.local>
2021-04-30 21:56:10 +0900

    프런트 script 연결

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use('/', home); // use -> 미들 웨어를 등록하는 메서드
 
 module.exports = app;
