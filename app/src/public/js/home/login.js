2421ffd6b9c5d185c4b26f16fbcbc9173c18d909
whitecrow <whitecrow@whitecrowui-Macmini.local>
2021-04-30 22:15:35 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,4 +12,12 @@ function login() {
         psword: psword.value,
     };
     console.log(req);
+
+    fetch('/login', {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req)
+    })
 }
