d68a8795c6c8afc2456f06b44760690a7839e267
whitecrow <whitecrow@whitecrowui-Macmini.local>
2021-05-05 11:19:06 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,21 @@
 'use strict';
 
-class UserStorage {
-  static #users = {
-    //바로접근 하지만 private(은닉화)
-    id: ['whitecrow', 'yuna', 'hajun'],
-    psword: ['1234', '1234', '1234'],
-    name: ['가', '나', '다'],
-  };
+const fs = require('fs').promises; //promise설정
 
+class UserStorage {
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const userKeys = Object.keys(users); // => [id, psword, name]
+    const userInfo = userKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
+  }
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -20,19 +26,17 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const userKeys = Object.keys(users); // => [id, psword, name]
-    const userInfo = userKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-
-    return userInfo;
+    // const users = this.#users;
+    return fs
+      .readFile('./src/databases/users.json')
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
