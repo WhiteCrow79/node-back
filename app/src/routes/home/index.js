332433ecacda1d18fde7f4430a7b076e8996fa4a
whitecrow <whitecrow@whitecrowui-Macmini.local>
2022-03-08 00:14:58 +0900

    index chang

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 const ctrl = require('./home.ctrl');
 
-router.get('/', ctrl.output.home);
+router.get('/', ctrl.output.login);
+router.get('/dev', ctrl.output.home);
 router.get('/login', ctrl.output.login);
 router.get('/register', ctrl.output.register);
 
